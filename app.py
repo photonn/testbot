917ade88f1ef1f1bb58c4d6008e9055798fff429
Miguel Angel Martin Gordillo <photonn@hotmail.com>
2025-07-31 11:25:06 +0200

    sample app with application insights loaded from env variable

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, request, jsonify
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+import logging
+import os
+
 
 app = Flask(__name__)
+
+# Configure Azure Application Insights
+connection_string = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING", "")
+logger = logging.getLogger(__name__)
+if connection_string:
+    logger.addHandler(AzureLogHandler(connection_string=connection_string))
+    logger.setLevel(logging.INFO)
 
 @app.route('/api/messages', methods=['POST'])
 def echo():
     data = request.json
     user_message = data.get('text', '')
     reply = {"type": "message", "text": f"You said: {user_message}"}
+    # Log the incoming message to Application Insights
+    logger.info(f"Received message: {user_message}")
     return jsonify(reply)
 
 if __name__ == '__main__':
